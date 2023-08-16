03ac6c016a800f09ab2f2484c048ec35391aae8d
Mariojoestar33 <mariano.pena15@gmail.com>
2023-08-16 13:43:43 -0600

    carrusel

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const grande = document.querySelector('.slider-box')
+const punto = document.querySelectorAll('.punto') 
+
+punto.forEach((cadaPunto, i) => {
+    punto[i].addEventListener('click', () => {
+        let posicion = i
+        let operacion = posicion * -50
+        grande.style.transform = `translateX(${operacion}%)`
+
+        punto.forEach((cadaPunto, i) => {
+            punto[i].classList.remove('active')
+        })
+        punto[i].classList.add('active')
+    })
+})
